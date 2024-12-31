21a71ea702c339472d3d25afc677df1bac3cd542
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2024-12-31 09:51:05 +0530

    update

--- 08-web-alb/data.tf
+++ 08-web-alb/data.tf
@@ -8,4 +8,9 @@ data "aws_ssm_parameter" "public_subnets_ids" {
 data "aws_ssm_parameter" "acm_certificate_arn" {
     name  = "/${var.project_name}/${var.environment}/acm_certificate_arn"
 }
-
+# this is for cloudfront
+resource "aws_ssm_parameter" "web_alb_dns_name" {
+  name  = "/${var.project_name}/${var.environment}/web_alb_dns_name"
+  type  = "String"
+  value = aws_lb.web.dns_name
+}
