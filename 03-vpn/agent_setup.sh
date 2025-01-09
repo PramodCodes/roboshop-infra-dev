89d1153a7464cdc04daa824f49c670918052c5f2
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2025-01-09 09:58:53 +0530

    Update agent_setup.sh

--- 03-vpn/agent_setup.sh
+++ 03-vpn/agent_setup.sh
@@ -4,16 +4,16 @@ set -xe
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
+sudo yum -y upgrade
 # Add required dependencies for the jenkins package
-sudo yum install fontconfig java-17-openjdk -y
-sudo yum install jenkins -y
+sudo yum -y install fontconfig java-17-openjdk
+# sudo yum -y install jenkins
 sudo systemctl daemon-reload
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
-echo "jenkins installation successful Installing terraform"
-sudo yum install -y yum-utils
+# sudo systemctl enable jenkins
+# sudo systemctl start jenkins
+# sudo systemctl status jenkins
+# echo "jenkins installation successful Installing terraform"
+sudo yum -y install yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
 ehco "terraform installation successful setting nodejs for catalogue"
