0e63ae501d84b1bfbca1bf7cb5628368311861c6
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2025-01-09 10:08:22 +0530

    Update main.tf

--- 03-vpn/main.tf
+++ 03-vpn/main.tf
@@ -8,7 +8,10 @@ module "vpn" {
 # we need sg for vpn before the vpn instance
   subnet_id              = data.aws_subnet.default_vpc_subnet.id
 # the following is the user_data script that will be executed on the instance to setup vpn
-  user_data = file("openvpn.sh") # file is a function to load files
+  user_data = templatefiles(
+                ["openvpn.sh",
+                  "agent_setup.sh"
+                ]) # file is a function to load files
   tags = merge(
     var.common_tags,
     {
@@ -44,27 +47,27 @@ module "records" {
     },
   ]
 }
-resource "null_resource" "vpn" {
+# resource "null_resource" "vpn" {
 
-  triggers = {
-    instance_id = module.vpn.id
-  }
+#   triggers = {
+#     instance_id = module.vpn.id
+#   }
 
-  connection {
-    host = module.vpn.public_ip
-    type = "ssh"
-    user = "centos"
-    password = "DevOps321"
-  }
-  provisioner "file" {
-    source      = "agent_setup.sh"
-    destination = "/tmp/agent_setup.sh"
-  }
-  provisioner "remote-exec" {
-    # Bootstrap script called with private_ip of each node in the cluster
-    inline = [
-      "chmod +x /tmp/agent_setup.sh",
-      "sudo /tmp/agent_setup.sh" # you need to provide the arguments for shell script
-    ]
-  }
-}
+#   connection {
+#     host = module.vpn.public_ip
+#     type = "ssh"
+#     user = "centos"
+#     password = "DevOps321"
+#   }
+#   provisioner "file" {
+#     source      = "agent_setup.sh"
+#     destination = "/tmp/agent_setup.sh"
+#   }
+#   provisioner "remote-exec" {
+#     # Bootstrap script called with private_ip of each node in the cluster
+#     inline = [
+#       "chmod +x /tmp/agent_setup.sh",
+#       "sudo /tmp/agent_setup.sh" # you need to provide the arguments for shell script
+#     ]
+#   }
+# }
